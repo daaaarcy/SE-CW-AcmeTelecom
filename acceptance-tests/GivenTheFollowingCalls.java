8b927a90032cd93530ffc1095ed1c4bec84f95d7
charles <czwkoh@gmail.com>
2012-11-23 17:44:13 +0000

    intermediate commit

---
+++ acceptance-tests/GivenTheFollowingCalls.java
@@ -0,0 +1,16 @@
+import fit.ColumnFixture;
+
+public class GivenTheFollowingCalls extends ColumnFixture {
+
+    public String Tariff;
+    public int Number;
+
+	@Override
+	public void reset() {
+
+	}
+	
+	@Override
+	public void execute() {
+	}
+}
---
+++ acceptance-tests/GivenTheFollowingCustomers.java
@@ -0,0 +1,16 @@
+import fit.ColumnFixture;
+
+public class GivenTheFollowingCustomers extends ColumnFixture {
+
+    public String Tariff;
+    public int Number;
+
+	@Override
+	public void reset() {
+
+	}
+	
+	@Override
+	public void execute() {
+	}
+}
