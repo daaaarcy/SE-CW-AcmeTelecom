ef987ec1d495050dcd32ef39f6476db6a5d0dbf3
Darcy Qiu <darcy.qiu@gmail.com>
2012-11-29 20:07:33 +0000

    Updated cost calculation for the new scheme.

--- src/com/acmetelecom/util/Calculator.java
+++ src/com/acmetelecom/util/Calculator.java
@@ -1,30 +1,74 @@
 package com.acmetelecom.util;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
 import com.acmetelecom.Call;
 import com.acmetelecom.DaytimePeakPeriod;
 import com.acmetelecom.customer.Tariff;
+import org.joda.time.DateTime;
 
-public class Calculator {
-	/**
-	 * 
-	 * @param call
-	 * @param tariff
-	 * @return
-	 */
-	public static BigDecimal calculateCost(Call call, Tariff tariff) {
-		BigDecimal cost;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
-		DaytimePeakPeriod peakPeriod = new DaytimePeakPeriod();
-		if (peakPeriod.offPeak(call.startTime()) && peakPeriod.offPeak(call.endTime()) && call.durationSeconds() < 12 * 60 * 60) {
-		    cost = new BigDecimal(call.durationSeconds()).multiply(tariff.offPeakRate());
-		} else {
-		    cost = new BigDecimal(call.durationSeconds()).multiply(tariff.peakRate());
-		}
+public class Calculator
+{
+    private static final int secondsInADay = 86400;
+    private static final int secondsInAnHour = 3600;
 
-		cost = cost.setScale(0, RoundingMode.HALF_UP);
-		return cost;
-	}
+    /**
+     * Calculates the cost of the call under the given tariff.
+     */
+    public static BigDecimal calculateCost(Call call, Tariff tariff)
+    {
+        final DateTime callStart = new DateTime(call.startTime());
+        final DateTime callEnd = new DateTime(call.endTime());
+
+        // Durations are all in seconds.
+        int peakCallDuration = 0;
+        final int totalCallDuration = call.durationSeconds();
+        final int callStartInSeconds = callStart.getSecondOfDay();
+
+        if (DaytimePeakPeriod.isOffPeak(callStart))
+        {
+            final int peakTimeStartSecondOfDay = DaytimePeakPeriod.start * secondsInAnHour;
+            int fromCallStartToPeakStart = peakTimeStartSecondOfDay - callStartInSeconds;
+
+            fromCallStartToPeakStart = (secondsInADay + fromCallStartToPeakStart) % secondsInADay;
+
+            if (fromCallStartToPeakStart < totalCallDuration)
+            {
+                int d = totalCallDuration - fromCallStartToPeakStart;
+                peakCallDuration = Math.min(DaytimePeakPeriod.peakDurationInSec(), d);
+            }
+        }
+        else
+        {
+            final int callEndInSeconds = callEnd.getSecondOfDay();
+
+            final int fromCallStartToPeakEnd = DaytimePeakPeriod.end * secondsInAnHour - callStartInSeconds;
+            final int fromCallEndToPeakEnd = DaytimePeakPeriod.end * secondsInAnHour - callEndInSeconds;
+
+            peakCallDuration = fromCallStartToPeakEnd;
+
+            if (callEndInSeconds >= callStartInSeconds)
+            {
+                if (fromCallEndToPeakEnd >= 0)
+                {
+                    peakCallDuration -= fromCallEndToPeakEnd;
+                }
+            }
+            else
+            {
+                final int fromPeakStartToCallEnd = callEndInSeconds - DaytimePeakPeriod.start * secondsInAnHour;
+                if (fromPeakStartToCallEnd >= 0)
+                {
+                    peakCallDuration += fromPeakStartToCallEnd;
+                }
+            }
+        }
+
+        BigDecimal peakCost = new BigDecimal(peakCallDuration).multiply(tariff.peakRate());
+        BigDecimal offPeakCost = new BigDecimal(totalCallDuration - peakCallDuration).multiply(tariff.offPeakRate());
+        BigDecimal cost =  peakCost.add(offPeakCost).setScale(0, RoundingMode.HALF_UP);
+
+        return cost;
+    }
 }
